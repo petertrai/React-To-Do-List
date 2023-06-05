5f45c7b925dbe12a7f03d468f171c8e5b69978b8
petertrai <petertrai@gmail.com>
2023-06-04 20:02:36 -0700

    finished todo list project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,70 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
+
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        },
+      ]
+    }
+  }
+  handleAdd = (task) => {
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    }
+    this.setState({
+     ...this.state, todos: [...this.state.todos, newTodo]
+    })
+  }
+
+  handleClear = () => {
+    console.log('clear');
+    this.setState({
+      ...this.state, todos: this.state.todos.filter(todo => {
+        if (!todo.completed) return todo
+      })
+    })
+  }
+
+  handleToggle = (id) => {
+    console.log(id);
+
+    this.setState({
+     ...this.state, todos: this.state.todos.map(todo => {
+      if (todo.id === id) {
+        return {...todo, completed: !todo.completed }
+      } return todo
+      
+     })
+    })
+  }
+
+
   render() {
+    const { todos } = this.state
+
+
     return (
       <div>
-        Todo App
+        <TodoList todos={todos} handleToggle={this.handleToggle}/>
+        <Form todos={todos} handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear Completed</button>
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputText: ''
+    }
+  }
+  handleChange = e => {
+    this.setState({
+     inputText: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAdd(this.state.inputText);
+    this.setState({ inputText: '' })
+  }
+
   render() {
+    const { inputText } = this.state;
+
     return (
-      <div>
-        Form
-      </div>
+      <form>
+        <label>
+          <input value={this.state.inputText} onChange={this.handleChange} ></input>
+          <button onClick={this.handleSubmit}>Add</button><br />
+        </label>
+      </form>
     )
   }
 }
